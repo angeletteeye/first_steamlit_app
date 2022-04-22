629ea3d3ad113da7ffb69a4412712e1a5c035076
angeletteeye <104221884+angeletteeye@users.noreply.github.com>
2022-04-22 12:41:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Mom\'s New healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & BlueberryOatmeal')
-streamlit.title('Kale and Spinach Smoothie')
-streamlit.title('egg')
+streamlit.text('Omega 3 & BlueberryOatmeal')
+streamlit.text('Kale and Spinach Smoothie')
+streamlit.text('egg')
